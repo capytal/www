241a8ec4263fc4a43b2790007755dd404e0360cc
Gustavo L de Mello (Guz) <contact@guz.one>
2024-11-01 15:42:47 -0300

    feat(htmx,about,pages): return anchor module for about page

---
+++ assets/lib/about-page.js
@@ -0,0 +1,72 @@
+/**
+ * Toggle a element to be hidden or not, changing "display" style,
+ * aria-disabled and aria-hidden.
+ * @param {Element} e - The element to toggle.
+ * @param {boolean} toggle - The state to put the element.
+ */
+function toggleElement(e, toggle) {
+	let style = e.getAttribute('style');
+
+	if (toggle) {
+		e.setAttribute('aria-hidden', 'false');
+		e.setAttribute('aria-disabled', 'false');
+		e.setAttribute('style', style
+			? style.replaceAll('display:none;', 'display:initial;')
+			: 'display:initial;',
+		);
+	}
+	else {
+		e.setAttribute('aria-hidden', 'true');
+		e.setAttribute('aria-disabled', 'true');
+		e.setAttribute('style', style
+			? style.replaceAll('display:initial;', 'display:none;')
+			: 'display:none;',
+		);
+	}
+}
+
+/**
+ * Hello world.
+ */
+export function main() {
+	console.debug('RUNNING');
+
+	const anchors = {
+		code: document.querySelector('#return-anchor-code'),
+		creators: document.querySelector('#return-anchor-creators'),
+		root: document.querySelector('#return-anchor-root'),
+	};
+
+	console.debug('AAAAA', anchors);
+
+	if (!anchors.code || !anchors.creators || !anchors.root) {
+		return;
+	}
+
+	console.debug('AAAAA');
+
+	/** @type {URL | undefined} */
+	let url = undefined;
+	try {
+		url = new URL(document.URL);
+	}
+	catch (error) {
+		console.error(`Failed to get document url. Error: ${String(error)}`);
+		url = new URL('/');
+	}
+
+	console.debug('hash', url.hash);
+
+	switch (url.hash) {
+		case '#code':
+			toggleElement(anchors.code, true);
+			toggleElement(anchors.creators, false);
+			toggleElement(anchors.root, false);
+			break;
+		case '#creators':
+			toggleElement(anchors.code, false);
+			toggleElement(anchors.creators, true);
+			toggleElement(anchors.root, false);
+			break;
+	}
+}
