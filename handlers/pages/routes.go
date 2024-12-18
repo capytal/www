8559f57473794760757edf5851be71fdb0dea7e9
Gustavo L de Mello (Guz) <contact@guz.one>
2024-12-18 15:51:51 -0300

    chore: fix commited file

--- handlers/pages/routes.go
+++ handlers/pages/routes.go
@@ -2,6 +2,7 @@ package pages
 
 import (
 	"log/slog"
+	"net/http"
 
 	"forge.capytal.company/loreddev/x/groute/router"
 	"forge.capytal.company/loreddev/x/groute/router/rerrors"
@@ -16,7 +17,7 @@ func Routes(log *slog.Logger) router.Router {
 	r.Handle("/about", &AboutPage{})
 
 	b := NewBlog("dot013", "blog", "https://forge.capytal.company/api/v1")
-	r.Handle("/blog/", b.Routes())
+	r.Handle("/blog", http.StripPrefix("/blog/", b.Routes()))
 
 	return r
 }
